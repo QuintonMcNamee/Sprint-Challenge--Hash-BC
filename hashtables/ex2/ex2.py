b9060d05ef89a7027c9ce16a5c7893dcfc0c5238
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2020-02-14 13:02:26 -0500

    ex2 complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -14,10 +14,21 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
-    route = [None] * length
+    route = []
 
-    """
-    YOUR CODE HERE
-    """
+    # YOUR CODE HERE
 
-    pass
+    # loop through tickets array
+    for ticket in tickets:
+        # insert each key(source) and value(destination) into the hashtable
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+    
+    # retrieve the first index
+    location = hash_table_retrieve(hashtable, "NONE")
+    print('LOCATION: ', location)
+
+    # while the location isnt "NONE", append the location to the route list and retrieve the next location
+    while location != "NONE":
+        route.append(location)
+        location = hash_table_retrieve(hashtable, location)
+    return route
