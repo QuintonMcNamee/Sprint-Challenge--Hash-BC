bb9f6ef65513378a02e00cc34ee642f98283acdc
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2020-02-14 12:03:18 -0500

    ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -7,13 +7,30 @@ from hashtables import (HashTable,
 
 
 def get_indices_of_item_weights(weights, length, limit):
-    ht = HashTable(16)
+    ht = HashTable(length)
+    
+    # YOUR CODE HERE
+    
+    # declare base case, if result is == None
+    result = None
 
-    """
-    YOUR CODE HERE
-    """
+    # loop through the weights array
+    for index, weight in enumerate(weights):
+        # if not the base case
+        if hash_table_retrieve(ht, weight) != None:
+            # retrieve hash table and key
+            result = [index, hash_table_retrieve(ht, weight)]
+            # result should have higher value in 0th index and smaller value in the 1st index. swap if not the case
+            if result[0] < result[1]:
+                temp = result[0]
+                result[0] = result[1]
+                result[1] = temp
+                return result
+        # finally, insert the key and value into the hash table
+        else:
+            hash_table_insert(ht, limit-weight, index)
 
-    return None
+    return result
 
 
 def print_answer(answer):
